318f8e2ec0b0f88c62fabfe76e9a0e38fb88b0c8
michal <michal@michal-virtual-machine>
2019-08-12 16:41:07 +0200

    12-08-2019

---
+++ LED_TESTER_V1/src/Include/Lux_Read_Live.h
@@ -0,0 +1,13 @@
+/*
+ * Lux_Read_Live.h
+ *
+ *  Created on: Aug 8, 2019
+ *      Author: michal
+ */
+
+#ifndef INCLUDE_LUX_READ_LIVE_H_
+#define INCLUDE_LUX_READ_LIVE_H_
+
+void lux_live_read(void);
+
+#endif /* INCLUDE_LUX_READ_LIVE_H_ */
---
+++ LED_TESTER_V1/src/Menus/Lux_Read_Live.c
@@ -0,0 +1,176 @@
+/*
+ * Lux_Read_Live.c
+ *
+ *  Created on: Aug 8, 2019
+ *      Author: michal
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <assert.h>
+#include <time.h>
+#include <stdint.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include "../LCD/iolib.h"
+#include "../LCD/beagle_gpio.h"
+#include "../LCD/beagle_hd44780.h"
+
+#define USBPath "\
+#!/bin/bash \n\
+path=`ls -l /dev/disk/by-id/usb* | tail -1 | tail -c 5` \n\
+mkdir ~/USBStick \n\
+mount /dev/$path ~/USBStick \n\
+"
+
+#define SaveTemp "\
+#!/bin/bash \n\
+d=`date +%F_%T` \n\
+temp=`cat /home/puka/Led_Tester_V1/TempTimeRead/BuffTemp.txt | tail -c 5` \n\
+fn=`ls -lt ~/USBStick | head -2 | grep --color=AUTO \"TempTimeRead.*.csv\"` \n\
+fn=${fn#*TempTimeRead} \n\
+echo \"$temp,$d\" >> \"/root/USBStick/TempTimeRead$fn\" \n\
+cat \"/root/USBStick/TempTimeRead$fn\" \n\
+"
+#define luxreadpy "sudo /usr/bin/python /root/LED_Tester_V1/LuxRead/lux.py"
+
+struct gpioID enabled_gpio[6];
+
+//Buttons Define
+int LXbuttonBack = 41;
+int LXbuttonEnter = 17;
+int LXbuttonPrev = 15;
+int LXbuttonNext = 16;
+
+float lux_read(void)
+{
+	int file;
+	char *filename = "/root/LED_Tester_V1/LuxRead/buf";
+	float lux=0;
+
+
+		system(luxreadpy);
+		sleep(1);
+		file = fopen(filename, "r");
+		fscanf(file, "%f", &lux);
+		if(!feof (file))
+		{
+			printf("\n Lux: %f", lux);
+		}else;
+
+		fclose(file);
+
+	return(lux);
+}
+
+void lux_live(void)
+{
+	float lux;
+	float buffMax;
+	float LuxMax;
+	float buffMin;
+	float LuxMin=5000;
+	char LuxMaxPrint[21];
+	char LuxMinPrint[21];
+	int refresh_rate_LCD;
+
+	int i=0;
+	float lux_all=0;
+	float lux_avg;
+	char LuxAvgPrint[21];
+
+	float time_el=0;
+	int time_hours=0;
+	int time_mins=0;
+
+	char lux_act_print[21];
+
+	//Clean LCD
+	clear_Screen(enabled_gpio);
+
+	while (is_low(9, LXbuttonBack))
+	{
+		//Read Lux
+		lux=lux_read();
+
+		//Refresh Rate
+		refresh_rate_LCD = 1;
+		i++;
+		sleep(refresh_rate_LCD);
+
+		//Actual Lux 1s interval
+		printf("\n Lux Act: %.1f \n\n", lux);
+		sprintf(lux_act_print, "Lux: %.1f lx ", lux);
+		goto_ScreenLine(0, enabled_gpio);
+		stringToScreen(lux_act_print, enabled_gpio);
+
+		//Max Lux
+		buffMax=lux;
+
+		if(LuxMax<=buffMax)
+		{
+			LuxMax=buffMax;
+			sprintf(LuxMaxPrint, "Max: %.1f lx ", LuxMax);
+			printf("\n %s", LuxMaxPrint);
+			goto_ScreenLine(1, enabled_gpio);
+			stringToScreen(LuxMaxPrint, enabled_gpio);
+		}
+		else
+		{
+		sprintf(LuxMaxPrint, "Max: %.1f lx ", LuxMax);
+		printf("\n %s", LuxMaxPrint);
+		goto_ScreenLine(1, enabled_gpio);
+		stringToScreen(LuxMaxPrint, enabled_gpio);
+		}
+
+		//Min Lux
+		buffMin=lux;
+		if(LuxMin>=buffMin)
+		{
+			LuxMin=buffMin;
+			sprintf(LuxMinPrint, "Min: %.1f lx ", LuxMin);
+			printf("\n %s", LuxMinPrint);
+			goto_ScreenLine(2, enabled_gpio);
+			stringToScreen(LuxMinPrint, enabled_gpio);
+		}
+		else
+		{
+			sprintf(LuxMinPrint, "Min: %.1f lx ", LuxMin);
+			printf("\n %s", LuxMinPrint);
+			goto_ScreenLine(2, enabled_gpio);
+			stringToScreen(LuxMinPrint, enabled_gpio);
+		}
+
+		//Lux Average
+		lux_all=lux_all+lux;
+		lux_avg=lux_all/i;
+
+		//Time Elapsed
+		time_el=time_el+2.72;
+		time_hours=time_el/3600;
+		time_mins=(time_el-(time_hours*3600))/60;
+		printf("\n Time Elapsed: %f Hours: %d Mins: %d", time_el, time_hours, time_mins);
+		sprintf(LuxAvgPrint, "Avg: %.1f lx |%d:%d| ", lux_avg, time_hours, time_mins);
+		printf("\n %s", LuxAvgPrint);
+		goto_ScreenLine(3, enabled_gpio);
+		stringToScreen(LuxAvgPrint, enabled_gpio);
+
+	}
+	//Press Back Button, return to KeyLoop
+	KeyLoop();
+}
+
+void lux_live_read(void)
+{
+	lux_live();
+}
